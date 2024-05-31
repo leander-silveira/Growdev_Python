a3ddcc904dcafc7f0c91968097638bfa4ee1b2e3
Leander P. Silveira <leander.silveira@altus.com.br>
2024-05-31 12:16:00 -0300

    estrutura de dados

---
+++ Bloco01_semana04_exercicio9_estrutura_de_dados.py
@@ -0,0 +1,10 @@
+'''
+Crie dois sets com cores diferentes e exiba na tela a união dos dois sets.
+'''
+
+set1 = {"azul", "vermelho", "amarelo"}
+set2 = {"verde", "roxo", "amarelo"}
+
+union = set1.union(set2)
+
+print(union)
