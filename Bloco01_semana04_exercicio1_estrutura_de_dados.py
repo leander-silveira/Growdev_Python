c9e412c69b9845be8ba789de02c093f1cb431d51
Leander P. Silveira <leander.silveira@altus.com.br>
2024-05-31 11:04:44 -0300

    estrutura de dados

---
+++ Bloco01_semana04_exercicio1_estrutura_de_dados.py
@@ -0,0 +1,14 @@
+'''
+1 - Crie uma lista com 100 números inteiros.
+
+Pode ser uma sequência, ou caso queira se desafiar faça de números aleatorios.
+Os dados não devem ser inseridos manualmente, utilize de um laço de repetição para adicionar os elementos a Lista.
+'''
+
+
+lista_num_int = []
+
+for i in range(1, 101):
+    lista_num_int.append(i)
+
+print(lista_num_int)
